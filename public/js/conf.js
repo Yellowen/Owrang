bc951465337d8a1fca915234b5c6607d1a63f0fd
Sameer Rahmani <lxsameer@gnu.org>
2013-10-11 13:22:07 +0330

    fa translation implemented

--- public/js/conf.js
+++ public/js/conf.js
@@ -5,8 +5,8 @@ wn.provide('erpnext');
 
 // add toolbar icon
 $(document).bind('toolbar_setup', function() {
-	wn.app.name = "ERPNext";
-	
+	wn.app.name = "Owrang";
+
 	var brand = ($("<div></div>").append(wn.boot.website_settings.brand_html).text() || 'erpnext');
 	$('.navbar-brand').html('<div style="display: inline-block;">\
 			<object type="image/svg+xml" data="app/images/splash.svg" class="toolbar-splash"></object>\
@@ -24,20 +24,20 @@ $(document).bind('toolbar_setup', function() {
 wn.provide('wn.ui.misc');
 wn.ui.misc.about = function() {
 	if(!wn.ui.misc.about_dialog) {
-		var d = new wn.ui.Dialog({title:'About ERPNext'})
-	
+		var d = new wn.ui.Dialog({title:'About Owrang'})
+
 		$(d.body).html(repl("<div>\
-		<p>ERPNext is an open-source web based ERP made by Web Notes Technologies Pvt Ltd.\
+		<p>Owrang is an open-source web based ERP made by Web Notes Technologies Pvt Ltd.\
 		to provide an integrated tool to manage most processes in a small organization.\
 		For more information about Web Notes, or to buy hosting servies, go to \
 		<a href='https://erpnext.com'>https://erpnext.com</a>.</p>\
-		<p>To report an issue, go to <a href='https://github.com/webnotes/erpnext/issues'>GitHub Issues</a></p>\
+		<p>To report an issue, go to <a href='https://github.com/Yellowen/Owrang/issues'>GitHub Issues</a></p>\
 		<hr>\
 		<p><a href='http://www.gnu.org/copyleft/gpl.html'>License: GNU General Public License Version 3</a></p>\
 		</div>", wn.app));
-	
-		wn.ui.misc.about_dialog = d;		
+
+		wn.ui.misc.about_dialog = d;
 	}
-	
+
 	wn.ui.misc.about_dialog.show();
 }
--- startup/__init__.py
+++ startup/__init__.py
@@ -2,17 +2,17 @@
 
 # ERPNext - web based ERP (http://erpnext.com)
 # Copyright (C) 2012 Web Notes Technologies Pvt Ltd
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -21,7 +21,7 @@ from __future__ import unicode_literals
 
 import webnotes
 
-product_name = "ERPNext"
+product_name = "Owrang"
 profile_defaults = {
 	"Company": "company",
 	"Territory": "territory"
@@ -30,9 +30,9 @@ profile_defaults = {
 application_home_page = "desktop"
 
 # add startup propertes
-mail_footer = """<div style="padding: 7px; text-align: right; color: #888"><small>Sent via 
-	<a style="color: #888" href="https://erpnext.com">ERPNext</a></div>"""
-	
+mail_footer = """<div style="padding: 7px; text-align: right; color: #888"><small>Sent via
+	<a style="color: #888" href="https://owrang.yellowen.com">Owrang</a></div>"""
+
 def get_monthly_bulk_mail_limit():
 	import webnotes
 	# if global settings, then 500 or unlimited
